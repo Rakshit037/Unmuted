2c6b7a488086854951539ce9fb1234a7f24d470e
Rakshit037 <rakshitdhimmar07@gmail.com>
2026-03-19 12:58:40 +0530

    Auth system with JWT, cookies, and role-based access

---
+++ backend/controllers/authController.js
@@ -0,0 +1,80 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Generate Token
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
+
+// REGISTER
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "User registered successfully!"
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// LOGIN
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(400).json({ message: "Invalid credentials!!" });
+    }
+
+    const token = generateToken(user);
+
+    // HTTP-only cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, // true in production (HTTPS)
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    res.json({
+      message: "Login successful!",
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// LOGOUT
+export const logoutUser = (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out successfully!" });
+};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import { protect } from "./middleware/authMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -22,6 +24,15 @@ app.use(cors({
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/test", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
 });
 
 const PORT = process.env.PORT || 5000;
